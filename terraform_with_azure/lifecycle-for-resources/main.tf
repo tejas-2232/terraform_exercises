4592cf787159d75e6b9e77bfe9d6ab9a9b7a3b8f
tejas-2232 <tejasbachhav98@gmail.com>
2022-03-07 11:50:52 +0000

    added code

---
+++ terraform_with_azure/lifecycle-for-resources/main.tf
@@ -0,0 +1,45 @@
+# configure microsoft azure provider
+provider "azurerm" {
+  features {}
+  #  subscription_id = "xxxxxxxxxxxxxxxxxxxxxx"
+  #  client_id = "xxxxxxxxxxxxxxxxxxxxxx"
+  #  client_secret = "xxxxxxxxxxxxxxxxxxxxxx"
+  #  tenant_id = "xxxxxxxxxxxxxxxxxxxxxx"
+}
+
+# create resource group
+resource "azurerm_resource_group" "expample_rg_blue" {
+    name = "BGD-Blue-RG"
+    location = "eastus"
+
+    tags = {
+      "owner" = "tejas b"
+      "team"  = "devops"
+      "project" = "Automation"
+    }
+}
+
+# this will prevent resource from being destroyed
+
+lifecycle{
+    prevent_destroy = true
+}
+
+# this will destory resources 
+
+# lifecycle{
+#     prevent_destroy = false
+# }
+
+# create 2nd resource group
+
+resource "azurerm_resource_group" "expample_rg_red" {
+    name = "BGD-GREEN-RG"
+    location = "eastus"
+
+    tags = {
+      "owner" = "tejas b"
+      "project" = "Blue-box"
+      "version" = "v1.2.0"
+    }
+}
