eed518cc051d0b6c92ec6957b0984347d27bb819
tejas-2232 <tejasbachhav98@gmail.com>
2022-03-09 07:22:50 +0000

    added code to provision multiple instance using vars

---
+++ terraform_with_azure/day3/01_multi_instances/variables.tf
@@ -0,0 +1,38 @@
+variable "my_loc" {
+  description = "My Resource Location"
+}
+
+variable "my_vm_count" {
+  default = 3
+}
+
+variable "my_rg_name" {
+  description = "My Resource Group Name"
+  default     = "TejasRG"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Administrator username for server"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Administrator password for server"
+}
+
+variable "managed_disk_type" {
+  type        = map
+  description = "Disk type Premium in Primary location Standard in DR location"
+
+  default = {
+    westus = "Premium_LRS"
+    eastus = "Standard_LRS"
+  }
+}
+
+variable "vm_size" {
+  type        = string
+  description = "Size of VM"
+  default     = "Standard_B1s"
+}
