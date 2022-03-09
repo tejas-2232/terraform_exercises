01d27d2e3b2f1b6a155954f73c8358fc342eaa32
tejas-2232 <tejasbachhav98@gmail.com>
2022-03-09 11:54:41 +0000

    added 4th condition

--- terraform_with_azure/day3/04-using_conditions/resource.tf
+++ terraform_with_azure/day3/04-using_conditions/resource.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "rg" {
     name = "RG-${var.system}-${count.index}"
     location = var.location[count.index]
-    count    = var.multi-region-deployment ? 3 : 1
+    count    = var.multi-region-deployment ? 4 : 2
     tags = {
         environment = "Terraform demo"
     } 
