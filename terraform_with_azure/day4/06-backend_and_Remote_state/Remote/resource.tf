1b7fcac3cdc5a8a92b13321e2ce77276ec4a2528
tejas-2232 <tejasbachhav98@gmail.com>
2022-03-10 12:32:08 +0000

    added resource file

---
+++ terraform_with_azure/day4/06-backend_and_Remote_state/Remote/resource.tf
@@ -0,0 +1,11 @@
+# Create a resource group
+resource "azurerm_resource_group" "example_rg_red" {
+  name     = "example-resource-RG"
+  location = "eastus"
+
+ tags = { 
+  owner = "Tejas-B"
+  # team  = "DevOps"
+  project = "Automation"
+  }
+}
